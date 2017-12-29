31195cf078f1b217d5b19bb9800fc6e06418bfbc
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2017-12-29 13:42:34 -0500

    instructions

---
+++ ex1film.rb
@@ -0,0 +1,19 @@
+documentry = "who killed the electric car"
+drama = "saving private ryan"
+comedy = "shrek"
+dramedy = "there's something about mary"
+
+
+puts " Respond YES if you like the movie or NO if you do not?"
+
+puts "documentry"
+response_documentry = gets.chomp
+
+puts "drama"
+response_drama = gets.chomp
+
+puts "comedy"
+response_comedy = gets.chomp
+
+puts "dramedy"
+response_dramedy = gets.chomp
