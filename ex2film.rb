5dc8670da8732e27ea570bf1e15325905c7736ff
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2017-12-29 14:18:00 -0500

    rating

---
+++ ex2film.rb
@@ -0,0 +1,22 @@
+# db entries to variables
+
+documentry = "who killed the electric car"
+drama = "saving private ryan"
+comedy = "shrek"
+dramedy = "there's something about mary"
+
+
+puts "Rate the following movie types from 1 to 5"
+
+#response stored as integer
+puts "documentry"
+response_documentry = gets.to_i
+
+puts "drama"
+response_drama = gets.to_i
+
+puts "comedy"
+response_comedy = gets.to_i
+
+puts "dramedy"
+response_dramedy = gets.to_i
