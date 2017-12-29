474bae84b0354dfd6b6af9e8390abb89240ea178
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2017-12-29 13:52:23 -0500

    instructions2

--- ex1film.rb
+++ ex1film.rb
@@ -6,14 +6,32 @@ dramedy = "there's something about mary"
 
 puts " Respond YES if you like the movie or NO if you do not?"
 
-puts "documentry"
+
 response_documentry = gets.chomp
+puts "documentry"
 
-puts "drama"
 response_drama = gets.chomp
+puts "drama"
 
-puts "comedy"
 response_comedy = gets.chomp
+puts "comedy"
 
-puts "dramedy"
 response_dramedy = gets.chomp
+puts "dramedy"
+
+if awnser_drama == "YES" && awnser_comedy == "YES"
+  puts "You should see #{dramedy}"
+
+elsif awnser_documentry == "YES"
+  puts "You need to see #{documentry}"""
+
+elsif awnser_comedy == "YES"
+  puts "You should try #{comedy}"
+
+elsif awnser_drama ==   "YES"
+  puts "You gotta watch #{drama}"
+
+else
+  puts "You should read 'The Hobbit'"
+
+end
